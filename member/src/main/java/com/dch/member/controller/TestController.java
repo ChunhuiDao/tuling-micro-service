a3695e4ec96f0c734a7080624a9769c3fa5b7f80
DCH <1148332999@qq.com>
2020-05-21 16:58:23 +0800

    ----

--- member/src/main/java/com/dch/member/controller/TestController.java
+++ member/src/main/java/com/dch/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.dch.member;
+package com.dch.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
